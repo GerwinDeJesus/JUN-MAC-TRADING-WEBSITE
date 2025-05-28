cfce0d6235b5e0cff13d6574310fb50dca549b48
GerwinDeJesus <dejesusgerwin64@gmail.com>
2025-05-28 11:44:34 +0800

    Replace <img> with next/image in Hero.tsx to improve performance

--- src/components/front-end/Hero.tsx
+++ src/components/front-end/Hero.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import Image from "next/image";
+
 const Hero = () => {
   return (
     <div className="bg-[#E3EDF6] mt-4">
@@ -13,19 +17,24 @@ const Hero = () => {
               Exclusive offer <span className="text-red-600">-10%</span> of every first time buyer
             </h3>
 
-           <a
-               className="inline-block bg-white rounded-md px-6 py-3 hover:bg-accent hover:text-white"
-               href="/contact"
-                  >
-                    Contact Us
-                  </a>
-
+            <a
+              className="inline-block bg-white rounded-md px-6 py-3 hover:bg-accent hover:text-white"
+              href="/contact"
+            >
+              Contact Us
+            </a>
 
           </div>
         </div>
 
         <div>
-          <img className="ml-auto" src="/bg1.png" alt="hero" />
+          <Image
+            src="/bg1.png"
+            alt="hero"
+            width={500}  // Set appropriate width
+            height={400} // Set appropriate height
+            className="ml-auto"
+          />
         </div>
       </div>
     </div>
